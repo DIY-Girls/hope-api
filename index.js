184609bd6a2f386ca253909ad8bef349e3af3935
Paul Luna <paulluna0215@gmail.com>
2020-01-16 18:59:32 -0800

    removed env variables

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const apiRoutes = require('./api.routes');
 
 const port = process.env.PORT || 8080;
 
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0-qxxtp.mongodb.net/test?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://paulluna0215:young2!5@cluster0-qxxtp.mongodb.net/test?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
