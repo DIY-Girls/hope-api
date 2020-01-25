ee3a6ce8d66e6fe94d5ccd5c73601e19eaefda30
Paul Luna <paulluna0215@gmail.com>
2020-01-25 08:55:20 -0800

    added bcrypt

--- user.controller.js
+++ user.controller.js
@@ -1,4 +1,5 @@
-User = require('./users.model');
+const bcrypt = require('bcrypt');
+const User = require('./users.model');
 
 // Handle index actions
 exports.index = function(req, res) {
@@ -18,20 +19,30 @@ exports.index = function(req, res) {
 };
 
 // Handle create user actions
-exports.create = function(req, res) {
-    const user = new User();
-    user.name = req.body.name ? req.body.name : user.name;
-    user.gender = req.body.gender;
-    user.email = req.body.email;
-    user.phone = req.body.phone;
-    user.password = req.body.password;
-    console.log(user);
-    user.save(function(err) {
-        res.json({
-            message: 'new user created',
-            data: user
+exports.create = async function(req, res) {
+    let user = await User.findOne({ email: req.body.email });
+
+    if(user) {
+        return res.status(400).send('User already exists');
+    } else {
+        user = new User();
+        user.name = req.body.name ? req.body.name : user.name;
+        user.gender = req.body.gender;
+        user.email = req.body.email;
+        user.phone = req.body.phone;
+        
+        // Hashing password
+        // const salt = await bcrypt.genSalt(10);
+        // user.password = await bcrypt.hash(req.body.password, salt);
+        user.password = req.body.password;
+        console.log(user);
+        user.save(function(err) {
+            res.json({
+                message: 'new user created',
+                data: user
+            });
         });
-    });
+    }
 };
 
 // Handle view contact info
