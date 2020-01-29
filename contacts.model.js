9ea7d71f7dfd7585961b24d2cb023ed1fef1cd05
Paul Luna <paulluna0215@gmail.com>
2020-01-28 21:11:43 -0800

    created model schema for emergency contacts

---
+++ contacts.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    }
+})
+
+module.exports = {
+    ContactSchema,
+}
+
+
