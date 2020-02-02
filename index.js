8c2909cd38f2b9227e875ba0782619b27371ee41
Paul Luna <paulluna0215@gmail.com>
2020-02-02 11:16:31 -0800

    fixing deployment error

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const apiRoutes = require('./api.routes');
 
 const port = process.env.PORT || 8080;
 
-PRODUCTION
+// PRODUCTION
 mongoose.connect(`mongodb+srv://paulluna0215:young2!5@cluster0-qxxtp.mongodb.net/test?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
