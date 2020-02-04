9d98b3716314f8a95bfe0fd0909641b60d72b42d
Paul Luna <paulluna0215@gmail.com>
2020-02-03 20:50:44 -0800

    Implemented login using async/await

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,29 +1,31 @@
+const debug = require('debug')('auth')
+
 User = require('../model/users.model');
 
-exports.login = function(req, res) {
-    console.log('authenticating...');
-    console.log(req.body);
-    User.findOne({email: req.body.email}, function(err, user) {
-        if(err || !user) {
-            res.status(403);
-            return res.json({
-                status: 'error',
-                message: 'Cannot access page!'
-            });
+exports.login = async function(req, res) {
+    debug('authenticating...');
+    debug(req.body);
+    try {
+        const user = await User.findOne({email: req.body.password});
+        if(!user) {
+            throw Error('Email is not on our system.');
         }
-        if(req.body.password !== user.password){
-            res.status(403);
-            return res.json({
-                status: 'error',
-                message: 'Passwords did not match ',
-            });
+        if(req.body.password !== user.password) {
+            debug('Passwords did not match');
+            throw Error('Credentials did not match');
         }
-
+        debug('passwords matched');
         return res.json({
             status: 'success',
             message: 'Logged in'
+        });
+    } catch(error) {
+        debug('An error occurred:', error);
+        return res.json({
+            status: 'error',
+            message: error
         })
-    })
+    }
 };
 
 exports.logout = function(req, res) {
